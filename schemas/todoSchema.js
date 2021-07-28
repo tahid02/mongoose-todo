e10b58c080f694c4faeea19da723da55798f6133
tahid02 <tanzim1463@gmail.com>
2021-07-28 15:22:36 +0600

    schema created

---
+++ schemas/todoSchema.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const todoSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String, // String is shorthand for {type: String}
+  title: {
+    type: String,
+    enum: ["active", "inactive"],
+  },
+  date: { type: Date, default: Date.now },
+  hidden: Boolean,
+});
