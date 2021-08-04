679cd653ff5d86af9f3d4ba1844f1f19e954be58
tahid02 <tanzim1463@gmail.com>
2021-08-04 16:55:42 +0600

    user schema defined

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const todoHandler = require("./routeHandler/todoHandler");
+const userHandler = require("./routeHandler/userHandler");
 
 // express app initialization
 const app = express();
@@ -19,6 +20,7 @@ mongoose
 // application route
 
 app.use("/todo", todoHandler);
+app.use("/user", userHandler);
 // app.use("/", todoHandler);
 
 // custom error handler
